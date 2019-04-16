11a404b635910971ac5963d1e5d3c5a186790434
mrm2004 <maiconrmartins@icloud.com>
2019-04-16 01:51:06 -0300

    Adicionadno os fontes do app

---
+++ lib/main.dart
@@ -0,0 +1,205 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart'; // para salvar dados no celular
+import 'dart:async';
+import 'dart:io';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+
+
+class _HomeState extends State<Home> {
+
+  List _toDoList = [];
+  final _toDoController = TextEditingController();
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
+  @override
+    void initState() {
+      // TODO: implement initState
+      super.initState();
+
+      _readData().then((data) {
+        setState(() {
+          _toDoList = json.decode(data);      
+        });
+        
+      });
+
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Lista de Tarefas"),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+      ),
+      body: new Column(
+        children: <Widget>[
+          new Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: new Row(
+              children: <Widget>[
+                
+                new Expanded( // faz com que o filho se expanda o maximo possivel, assim evita erro de infinite width. ele vai expandir ate encontrar outro whidgt ou o final da tela
+                  child: new TextField(
+                    decoration: InputDecoration(
+                      labelText: "Nova Tarefa",
+                      labelStyle: TextStyle(color: Colors.green)
+                    ),
+                    controller: _toDoController,
+                  ),
+                ),
+
+                new RaisedButton(
+                  color: Colors.green,
+                  child: Text("Add"),
+                  textColor: Colors.white,
+                  onPressed: _addToDo,
+                ),
+
+              ],
+            ),
+          ),
+          new Expanded(
+            child: RefreshIndicator(
+              child: new ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: buildItem
+              ),
+              onRefresh: _refresh,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()), // apensas uma key aleatoria
+      background: new Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white,),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (check) {
+          setState(() {
+            _toDoList[index]["ok"] = check;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+          
+          final snack = SnackBar(
+            content: Text("Tarefa ${_lastRemoved["title"]} removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+
+                setState(() {
+                   _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();               
+                });
+              },
+            ),
+            duration: Duration(seconds: 2),
+          );
+
+          Scaffold.of(context).showSnackBar(snack);
+
+        });
+
+      },  
+    );
+  }
+
+
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory(); // para pegar o directorio que ira salvar os dados
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    
+    String data = json.encode(_toDoList); // transformando a lista em json
+    final file = await _getFile();
+    return file.writeAsString(data);
+
+  }
+
+  Future<String> _readData() async {
+
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+
+  }
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newtoDo = Map();
+      newtoDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newtoDo["ok"] = false;
+      _toDoList.add(newtoDo);          
+      _saveData();
+    });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    
+    setState(() {
+      _toDoList.sort( (a, b) {
+        if (a["ok"] && !b["ok"]) 
+          return 1;
+        else if (!a["ok"] && b["ok"]) 
+          return -1;
+        else 
+          return 0;
+      });
+      
+      _saveData();      
+
+    });
+    return null;
+  }
+
+}
+
